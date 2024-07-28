0c447ed0c9778ef889882c894ec063b0e2d152c0
RootBeer8 <109646119+jojotru@users.noreply.github.com>
2024-07-27 21:27:25 -0400

    x

--- config/corsOptions.js
+++ config/corsOptions.js
@@ -1,21 +1,15 @@
 const allowedOrigins = require('./allowedOrigins')
 
-// const corsOptions = {
-//     origin: (origin, callback) => {
-//         if (allowedOrigins.indexOf(origin) !== -1 || !origin) {
-//             callback(null, true)
-//         } else {
-//             callback(new Error('Not allowed by CORS'))
-//         }
-//     },
-//     credentials: true,
-//     optionsSuccessStatus: 200
-// }
-
-const corsOptions = require('cors');
-app.use(corsOptions({
-    origin: 'https://abc-school-system.onrender.com', // Allow only your frontend domain
-    credentials: true // Enable this if you need to send cookies with requests
-}));
+const corsOptions = {
+    origin: (origin, callback) => {
+        if (allowedOrigins.indexOf(origin) !== -1 || !origin) {
+            callback(null, true)
+        } else {
+            callback(new Error('Not allowed by CORS'))
+        }
+    },
+    credentials: true,
+    optionsSuccessStatus: 200
+}
 
 module.exports = corsOptions 
